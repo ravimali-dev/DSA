2f2bfeeec98aa03eef8ae099136dcf5f199368d7
Ravi Mali <ravimalakar091@gmail.com>
2026-03-29 13:55:37 +0530

    solove a array question find average of array

--- ARRAY/app.js
+++ ARRAY/app.js
@@ -56,15 +56,26 @@
 // }
 // console.log(reversArr)
 
-//Find smallest element
+//Question 6: Find smallest element
 
-let arr =  [3, 7, 2, 9, 5]
+// let arr =  [3, 7, 2, 9, 5]
 
-let smallest = arr[0];
+// let smallest = arr[0];
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i]<smallest){
+//         smallest = arr[i]
+//     }
+// }
+
+// console.log(smallest);
+
+
+//Question 7: Find average of array
+
+let arr = [3, 7, 2, 9, 5];
+let sum = 0;
 for(let i=0; i<arr.length; i++){
-    if(arr[i]<smallest){
-        smallest = arr[i]
-    }
+    sum += arr[i]
 }
+console.log(sum/arr.length)
 
-console.log(smallest);
