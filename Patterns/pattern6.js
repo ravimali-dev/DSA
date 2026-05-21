e7bc1bfd569dcf104b7edbc3ff34f97864af1adc
Ravi Mali <ravimalakar091@gmail.com>
2026-05-21 13:10:02 +0530

    solove a new pattern printing problem

---
+++ Patterns/pattern6.js
@@ -0,0 +1,18 @@
+let n = 5;
+
+for(let i=0; i<n; i++){
+
+    let newLine = "";
+
+     for(let j=0; j<=n-i-1; j++){
+              newLine += " ";
+        }
+     for(let j=0; j<i*2-1; j++){
+        newLine += "*";
+     }
+      for(let j=0; j<=n-i-1; j++){
+              newLine += " ";
+        }
+        console.log(newLine)
+      
+}
