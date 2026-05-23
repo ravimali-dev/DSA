378bcad4abeb17b0e7b07b162000b401b4fa4666
Ravi Mali <ravimalakar091@gmail.com>
2026-05-23 11:33:21 +0530

    Solve number pattern printing problem

---
+++ Patterns/pattern7.js
@@ -0,0 +1,9 @@
+ let n = 5;
+ 
+ for(let i=1; i<=n; i++){
+            let newLine = '';
+            for(let j=0; j<i; j++){
+                newLine += i
+            }
+           console.log(newLine)
+        }
