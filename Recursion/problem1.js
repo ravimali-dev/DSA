6514a8848ba9a31d798607229a73faca1a89eb5c
Ravi Mali <ravimalakar091@gmail.com>
2026-07-23 20:48:39 +0530

    resolve problem 1 in recursion

--- Recursion/problem1.js
+++ Recursion/problem1.js
@@ -1,24 +1,12 @@
 
-// function print(){
-//     if(count > 5) return;
-
-//     console.log(count)
-//     count++;
-//     print()
-   
-// }
-// print()
-let count = 1;
-let sum = 0
 class Solution{
-    printNumbers(n){
-        if(count > n) return;
-        sum += count
-        count++
-        this.printNumbers(n)
+    printNumbers(i,n){
+        if(i > n) return;
+
+        console.log(i)
+        this.printNumbers(i + 1, n)
     }
 
 }
 const obj = new Solution();
-obj.printNumbers(10)
-console.log(sum)
+obj.printNumbers(1, 5)
