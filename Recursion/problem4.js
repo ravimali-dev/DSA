edd662af82a4762e0ad2179e0f581ff2cfbcef9b
Ravi Mali <ravimalakar091@gmail.com>
2026-08-08 12:59:25 +0530

    fix: update factorial base case to handle n = 0 and adjust test case

--- Recursion/problem4.js
+++ Recursion/problem4.js
@@ -1,6 +1,6 @@
 class Solution {
   factorial(n) {
-    if (n == 0) {
+    if (n <= 1) {
       return 1;
     }
 
@@ -9,4 +9,4 @@ class Solution {
 }
 const sol = new Solution();
 
-console.log(sol.factorial(4));
+console.log(sol.factorial(5));
