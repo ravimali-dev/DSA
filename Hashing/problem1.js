2eb351aa0430201b0b10e00c4b5805d0410e7d70
Ravi Mali <ravimalakar091@gmail.com>
2026-08-14 23:51:44 +0530

    refactor: remove unnecessary console log and add spacing for readability

--- Hashing/problem1.js
+++ Hashing/problem1.js
@@ -3,7 +3,6 @@ let arr = [10, 20, 10, 30, 20, 10];
 let myMap = new Map();
 
 for(let i=0; i<arr.length; i++){
-    console.log(myMap.has(arr[i]))
     if(myMap.has(arr[i])){
       let oldValue =  myMap.get(arr[i])
        myMap.set(arr[i], oldValue+1)
@@ -12,4 +11,9 @@ for(let i=0; i<arr.length; i++){
         myMap.set(arr[i], 1)
     }
 }
+
+
+
+
+
 console.log(myMap)
