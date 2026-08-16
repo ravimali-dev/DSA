59fcabf13fa293a95f02d7e22223ab0ae25d2917
Ravi Mali <ravimalakar091@gmail.com>
2026-08-16 23:22:17 +0530

    refactor: optimize two-sum implementation by using a Map for complement tracking

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -1,12 +1,13 @@
 let arr = [2, 7, 11, 15];
 let target = 9;
+let map = new Map()
 
 for (let i = 0; i < arr.length; i++) {
-    for (let j = i+1; j < arr.length; j++) {
-        if(arr[i] + arr[j] == target){
-            console.log(i,j)
-        }
-        
+    let complement = target - arr[i];
+
+    if(map.has(complement)){
+        console.log(map.get(complement), i)
     }
+    map.set(arr[i], i)
     
 }
